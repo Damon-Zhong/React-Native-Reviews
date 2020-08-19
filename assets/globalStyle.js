fdb16e504802e905595240a02f6d1995f19f80e0
Yuqi Zhong <damon.yq.zhong@gmail.com>
2020-08-19 10:29:18 -0400

    Added global styles module

---
+++ assets/globalStyle.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        padding: 20
+    },
+    titleText: {
+        fontFamily: 'DoHyeon',
+        fontSize: 18,
+        color: '#333'
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20
+    }
+})
